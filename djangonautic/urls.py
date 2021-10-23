da8905d2a5bc4b919e9c4466125bab8bc605a83e
shashannkd <shashannkd@msitprogram.net>
2021-10-24 00:03:32 +0530

    home and about URLs created

--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^about/$', ),
-    path(r'^$', ),
+    path('about/', views.about),
+    path('', views.homepage),
 ]
---
+++ djangonautic/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+
+
+def homepage(request):
+    return HttpResponse('Home Page')
+
+
+def about(request):
+    return HttpResponse('about')
