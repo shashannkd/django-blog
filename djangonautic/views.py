41b04becfa6c04ade95e7c85e2f704bf045e6398
shashannkd <shashannkd@msitprogram.net>
2021-10-24 00:13:21 +0530

    returning templates for home and about page

--- djangonautic/views.py
+++ djangonautic/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def homepage(request):
-    return HttpResponse('Home Page')
+    # return HttpResponse('Home Page')
+    return render(request, 'homepage.html')
 
 
 def about(request):
-    return HttpResponse('about')
+    # return HttpResponse('about')
+    return render(request, 'about.html')
